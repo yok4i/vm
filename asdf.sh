da65d0ca5009663155f3f2c6a7ba92660ad46685
Mayk <7395296+yok4i@users.noreply.github.com>
2022-04-13 10:42:20 -0300

    Make asdf script install dependencies

--- asdf.sh
+++ asdf.sh
@@ -4,6 +4,11 @@ echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
+# install system dependencies for python
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 asdf plugin add python
 asdf install python latest
 asdf install python latest:3.9
