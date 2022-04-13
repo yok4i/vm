69cc67bfb75f37d84c0899fefd6a303b1b170158
Mayk <7395296+yok4i@users.noreply.github.com>
2022-04-13 10:20:34 -0300

    Add asdf install script

---
+++ asdf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+asdf plugin add python
+asdf install python latest
+asdf install python latest:3.9
